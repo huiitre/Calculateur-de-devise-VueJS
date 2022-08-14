4c637d9469a91f285f1c87e624f0d823e5e0eee5
huiitre <yanis.deluca@laposte.net>
2022-08-14 23:23:23 +0200

    remove 404 et test build apk avec run android

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  outputDir: './src-cordova/www',
+  publicPath: './',
+
+  pluginOptions: {
+    cordovaPath: 'src-cordova'
+  }
 });
