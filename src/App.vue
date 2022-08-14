e29beaa276079bbb572f6adeabda5482555a4a4b
huiitre <yanis.deluca@laposte.net>
2022-08-15 00:13:38 +0200

    mise en commentaire du routage pour l'instant (app android)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 //* Affichage des vues de notre router
 <template>
+  //? pour l'appli mobile on met en commentaire le routage et on affiche direct le composant
   <!-- <RouterView /> -->
   <ConverterView />
 </template>
